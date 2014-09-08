af85f32cf506b5ef525d0718a89f773022cf8117
sillymoose <davidnmfarrell@gmail.com>
2014-09-07 22:42:18 -0400

    fixed type reference

--- examples/collision.c
+++ examples/collision.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "vector2d.h"
 
-Vector2d add_vector (Vector2d v1, Vector2d v2)
+Vector2D add_vector (Vector2D v1, Vector2D v2)
 {
-    Vector2d v = {v1.x + v2.x, v1.y + v2.y};
+    Vector2D v = {v1.x + v2.x, v1.y + v2.y};
     return v;
 }
